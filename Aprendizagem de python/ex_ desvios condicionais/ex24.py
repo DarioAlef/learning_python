ed8042060253c3409d9c0fbbe8b771b06cd4456d
Dário Alef <dario.alef@gmail.com>
2024-10-01 01:47:38 -0400

    2° files organization

--- Aprendizagem de python/ex_ desvios condicionais/ex24.py
+++ Aprendizagem de python/ex_ desvios condicionais/ex24.py
@@ -3,8 +3,8 @@ altura = float(input("Digite a sua altura em cm: "))
 idade = int(input("Digite a sua idade: "))
 genero = str(input("homem ou mulher? "))
 
-tmbH = 88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * idade)
-tmbM = (10 * peso) + (6.25 * altura) - (5 * idade) - 161
+tmbH = 88.362 + (13.397*peso) + (4.799*altura) - (5.677*idade)
+tmbM = (10*peso) + (6.25*altura) - (5*idade) - 161
 
 if genero == "homem":
     print("Sua Tx. metabólica basal é: ", tmbH)
---
+++ Aprendizagem de python/ex_ laços de repetição/ex_LR_01.py
@@ -0,0 +1,4 @@
+# Q.01 - Contar até 100: Use um laço para imprimir números de 1 a 100. (Usando o laço Para)
+
+for i in range(1, 101):
+    print(i)
