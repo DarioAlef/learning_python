9728bd43fe75b9bb917d89fab44f6084b5847bab
Dário Alef <155563578+DarioAlef@users.noreply.github.com>
2024-10-02 20:09:59 -0400

    Update ex_LR_12.py

--- Aprendizagem de python/ex_ laços de repetição/ex_LR_12.py
+++ Aprendizagem de python/ex_ laços de repetição/ex_LR_12.py
@@ -9,10 +9,3 @@ while num > 1:         # antes com 'num / 10', a variável 'num' não recebia o 
     num = num / 10     # ou seja, a cada novo laço, num voltava para o seu valor original
     count+=1           # por isso estava em um laço infinito
 print(count)
-=======
-while num > 0:
-    num / 10
-    count+=1
-    print(count)
-    break
->>>>>>> c3cc52410a1ffec836c65f0d646a8c97fcaaac57
