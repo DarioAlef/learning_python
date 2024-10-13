459bd1d5c3eb8948503d81a61496a4b23207d783
Dário Alef <dario.alef@gmail.com>
2024-10-13 17:43:07 -0400

    new

--- ANOTAÇÕES/variaveis_compostas.py
+++ ANOTAÇÕES/variaveis_compostas.py
@@ -18,5 +18,25 @@ lista_range = list(range(10))
 print(lista_range)
 print(lista[-1])
 
-ultimo = lista.pop()
-print(ultimo)
+    ### TUPLAS ### 
+minha_tupla = ('minha_tupla: ',1,2,3,4,5,'ai',True,7.75)
+print(minha_tupla)
+print(minha_tupla[0])
+print(minha_tupla[-1])
+print(minha_tupla.count(3))
+print(minha_tupla.index(7.75))
+print(len(minha_tupla))
+
+    ### DICIONÁRIOS ###
+minha_dict = {'nome':'Joaquim','sobrenome':'Silva','idade':30}
+print('dict:', minha_dict['idade'])
+dicionario_vazio = {}
+print(dicionario_vazio)
+print(minha_dict.keys())
+print(minha_dict.values())
+print(minha_dict.items())
+minha_dict['idade'] = 31  #forma para alterar um item do dict
+print(minha_dict)
+print(minha_dict.get('idade')) #forma mais eficientepara pegar um item do dict
+minha_dict.update({'nome':'Joãozinho','sobrenome':'Macaco','idade':40})
+print(minha_dict)
